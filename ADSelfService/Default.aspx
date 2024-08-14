1926e5fa2e3653a188ed44758261edc5d8ed1864
jack7 <jack7@GARY-PC>
2024-08-14 22:33:33 +0800

    加入專案檔案。

---
+++ ADSelfService/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ADSelfService.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ADSelfService</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>ADSelfService</h2>
+            <label for="username">Username:</label>
+            <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            <br />
+
+            <asp:Button ID="btnUnlock" runat="server" Text="Unlock Account" OnClick="btnUnlock_Click" />
+            <asp:Button ID="btnResetPassword" runat="server" Text="Reset Password" OnClick="btnResetPassword_Click" />
+            <br />
+
+            <asp:Label ID="lblMessage" runat="server" CssClass="message"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADSelfService/Default.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace ADSelfService
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private const string DefaultPassword = "!QAZ2wsx"; // Default Passowrd
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUnlock_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            if (UnlockUserAccount(username))
+            {
+                lblMessage.Text = "Account unlock successful.";
+            }
+            else
+            {
+                
+            }
+        }
+
+        protected void btnResetPassword_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            if (ResetUserPassword(username, DefaultPassword))
+            {
+                lblMessage.Text = "Password reset successful.";
+            }
+            else
+            {
+                lblMessage.Text = "Password reset failed.";
+            }
+        }
+
+        private bool UnlockUserAccount(string username)
+        {
+            try
+            {
+                using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+                {
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+                    if (user != null)
+                    {
+                        if (user.IsAccountLockedOut())
+                        {
+                            user.UnlockAccount();
+                            user.Save();
+                            lblMessage.Text = "Account unlock successful.";
+                            return true;
+                        }
+                        else
+                        {
+                            lblMessage.Text = "Account is not locked.";
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        lblMessage.Text = "User not found.";
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error (not shown).
+                lblMessage.Text = "Account unlock failed.";
+                return false;
+            }
+        }
+
+        private bool ResetUserPassword(string username, string newPassword)
+        {
+            try
+            {
+                using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+                {
+                    UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+                    if (user != null)
+                    {
+                        user.SetPassword(newPassword);
+                        user.ExpirePasswordNow(); // Change password next time
+                        user.Save();
+                        return true;
+                    }
+                    else
+                    {
+                        lblMessage.Text = "User not found.";
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error (not shown).
+                lblMessage.Text = "Password reset failed.";
+                return false;
+            }
+        }
+    }
+}
